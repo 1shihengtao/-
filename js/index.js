50a667ef16df45b19d9eec237638f9113c11dfd0
xinsui <xinsui@.com>
2022-09-02 14:01:22 +0800

    日常更新

--- js/index.js
+++ js/index.js
@@ -1,14 +1,22 @@
-// 轮播图
-var swiper = new Swiper('.mySwiper', {
-  loop: true,
-  spaceBetween: 30,
-  effect: 'fade',
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-  pagination: {
-    el: '.swiper-pagination',
-    clickable: true,
-  },
+$(function () {
+  // 轮播图
+  var swiper = new Swiper('.mySwiper', {
+    loop: true,
+    spaceBetween: 30,
+    effect: 'fade',
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+  })
+  // 导航栏
+  $('.xuanze li').eq(0).hover(function () {
+    $('.xuanze li').find('.caidan').show().slideDown()
+  }, function () {
+    
+  })
 })

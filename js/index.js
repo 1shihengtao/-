5936d7fbb1caab2348ba2ef6aebf74934f296448
xinsui <xinsui@.com>
2022-09-02 11:32:56 +0800

    日常更新

---
+++ js/index.js
@@ -0,0 +1,14 @@
+// 轮播图
+var swiper = new Swiper('.mySwiper', {
+  loop: true,
+  spaceBetween: 30,
+  effect: 'fade',
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+})

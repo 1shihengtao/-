87dda993c132854f446d14c6ff203df218389175
xinsui <xinsui@.com>
2022-09-04 13:14:53 +0800

    日常更新

--- js/login.js
+++ js/login.js
@@ -20,4 +20,51 @@ $(function () {
   $('form .Pyan').on('click', function () {
     $(this).find('img').fadeToggle()
   })
+  $('.Right article')
+    .eq(0)
+    .find('div a')
+    .eq(1)
+    .on('click', function (e) {
+      $('.Right article').eq(1).fadeToggle().find('div a').eq(1).addClass('pub').end().siblings().addClass('pubs')
+      return false
+    })
+  // 注册
+  $('.Right article')
+    .eq(1)
+    .find('button')
+    .on('click', function () {
+      $.ajax({
+        type: 'get',
+        url: 'http://jx.xuzhixiang.top/ap/api/reg.php',
+        data: {
+          username: $(this).prev().find('input').eq(1).val(),
+          password: $(this).prev().find('input').eq(2).val(),
+        },
+      }).then((res) => {
+        if (res.code == 1) {
+          alert('注册成功')
+        }
+      })
+    })
+  // 登录
+  $('.Right article')
+    .eq(0)
+    .find('button')
+    .on('click', function () {
+      $.ajax({
+        type: 'get',
+        url: 'http://jx.xuzhixiang.top/ap/api/login.php',
+        data: {
+          username: $(this).prev().find('input').eq(0).val(),
+          password: $(this).prev().find('input').eq(1).val(),
+        },
+      }).then((res) => {
+        if (res.code == 1) {
+          localStorage.setItem({ UserData: JSON.parse(res.data) })
+          location.assign('../index.html')
+        } else {
+          alert('用户名或密码不存在，请先注册后使用')
+        }
+      })
+    })
 })

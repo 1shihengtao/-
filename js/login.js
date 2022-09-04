866a153d31da3d36e5e00ff76304468739853ee1
xinsui <xinsui@.com>
2022-09-04 11:28:40 +0800

    日常更新

--- js/login.js
+++ js/login.js
@@ -9,4 +9,15 @@ $(function () {
         $(this).css({ color: '#838383' }).siblings().slideUp()
       }
     )
+  $('article figure').hover(
+    function () {
+      $(this).find('big').css({ opacity: 1 }).end().next().stop().fadeIn()
+    },
+    function () {
+      $(this).find('big').css({ opacity: 1 }).end().next().stop().fadeOut()
+    }
+  )
+  $('form .Pyan').on('click', function () {
+    $(this).find('img').fadeToggle()
+  })
 })
